b9e9eaacab8abdffd00fec9b0ff550b57d5ac690
carlosdelval <carlospnt96@gmail.com>
2025-05-13 13:40:12 +0200

    p

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -63,6 +63,37 @@ const Navbar = () => {
           </span>
         </a>
         <div className="flex items-center space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
+          {!user && (
+            <ul className="flex flex-col p-4 mt-4 font-medium rounded-lg md:p-0 md:space-x-8 md:flex-row md:mt-0">
+              <li>
+                <a
+                  href="/login"
+                  className="block px-3 py-2 m-2 duration-300 cursor-pointerblock rounded-xl hover:text-chryslerblue hover:dark:text-vistablue dark:text-babypowder"
+                >
+                  <div className="flex items-center space-x-2">
+                    <svg
+                      className="w-5 h-5"
+                      aria-hidden="true"
+                      xmlns="http://www.w3.org/2000/svg"
+                      width="24"
+                      height="24"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                    >
+                      <path
+                        stroke="currentColor"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth="2"
+                        d="M12 21a9 9 0 1 0 0-18 9 9 0 0 0 0 18Zm0 0a8.949 8.949 0 0 0 4.951-1.488A3.987 3.987 0 0 0 13 16h-2a3.987 3.987 0 0 0-3.951 3.512A8.948 8.948 0 0 0 12 21Zm3-11a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                      />
+                    </svg>
+                    <span>Iniciar sesión</span>
+                  </div>
+                </a>
+              </li>
+            </ul>
+          )}
           {user && (
             <div className="relative flex items-center space-x-3 md:order-2 md:space-x-0 rtl:space-x-reverse">
               <button
